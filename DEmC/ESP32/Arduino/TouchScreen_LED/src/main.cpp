f7bf45759d0b90fda6c054efb3639b7a20edb694
Mateo <mateo.avalos3942@gmail.com>
2024-05-07 09:37:47 -0500

    Added a missing line in the code

--- DEmC/ESP32/Arduino/TouchScreen_LED/src/main.cpp
+++ DEmC/ESP32/Arduino/TouchScreen_LED/src/main.cpp
@@ -8,10 +8,12 @@ void setup() {
 }
 
 void loop() {
-  int data = Serial2.read();
-  if (data == 3) {
-    digitalWrite(led, HIGH);
-  } else if (data == 4) {
-    digitalWrite(led, LOW);
+  if(Serial2.available() > 0) {
+    int data = Serial2.read();
+    if (data == 3) {
+      digitalWrite(led, HIGH);
+    } else if (data == 4) {
+      digitalWrite(led, LOW);
+    }
   }
 }
