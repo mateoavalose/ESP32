76fce671aaede282787e526dcb649b07b9a1a6cc
Mateo <mateo.avalos3942@gmail.com>
2024-05-09 00:05:51 -0500

    LM35 T° Sensor added. Not tested

---
+++ Examples/LM35_TemperatureSensor/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int tempPin = 34;
+int ADC = 0;
+int mV = 0;
+int temp = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(tempPin, INPUT);
+}
+
+void loop() {
+  ADC = analogRead(tempPin);
+  mV *= (3300.0 / 4096.0);
+  temp = mV / 10;
+  Serial.print("\nADC: "); Serial.println(ADC);
+  Serial.print(" Temp: "); Serial.print(temp); Serial.println(" C");
+  delay(1000);
+}
