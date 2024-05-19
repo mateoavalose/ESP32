54afc758882a5ee3383dfcab1cfbae05983df6cf
Mateo <mateo.avalos3942@gmail.com>
2024-05-18 19:05:26 -0500

    Modified ADC max value

--- Examples/LM35_TemperatureSensor/src/main.cpp
+++ Examples/LM35_TemperatureSensor/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const int tempPin = 34;
+const int tempPin = 26;
 int ADC = 0;
 int mV = 0;
 int temp = 0;
@@ -12,7 +12,7 @@ void setup() {
 
 void loop() {
   ADC = analogRead(tempPin);
-  mV = ADC*(3300.0 / 4096.0);
+  mV = ADC*(3300.0 / 4095.0);
   temp = mV / 10;
   Serial.print("\nADC: "); Serial.println(ADC);
   Serial.print(" Temp: "); Serial.print(temp); Serial.println(" C");
